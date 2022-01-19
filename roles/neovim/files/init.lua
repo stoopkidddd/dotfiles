e22af648ddee6984af3c37a5c859700c7214af79
Matt DeMichele <mattdemichele@gmail.com>
2022-01-18 19:58:19 -0500

    change neovim config

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,1 +1,57 @@
-require 'TheAltF4Stream'.init()
+require('basics')
+require('colors')
+require('telescope-config')
+require('coc-config')
+require('lualine').setup()
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  }
+}
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'folke/tokyonight.nvim'
+  use 'nvim-treesitter/nvim-treesitter' 
+  use 'tpope/vim-commentary'
+  use 'voldikss/vim-floaterm'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'ThePrimeagen/git-worktree.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    "blackCauldron7/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {
+      view = {
+        width = 5,
+        auto_resize = true
+      }
+    } end
+  }
+end)
