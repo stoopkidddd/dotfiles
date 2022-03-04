123b547924bbfb95380a920b686a0b4ce77b5de5
mattdemichele-pearson <matthew.demichele@pearson.com>
2022-03-04 11:40:39 -0500

    fix jsonls error. not sure if this is actually working as intended now though?

--- roles/neovim/files/lua/user/lsp/settings/jsonls.lua
+++ roles/neovim/files/lua/user/lsp/settings/jsonls.lua
@@ -2,6 +2,8 @@ local default_schemas = nil
 local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
 if status_ok then
   default_schemas = jsonls_settings.get_default_schemas()
+else
+  default_schemas = {} 
 end
 
 local schemas = {
